1ef78f37e4417fac8a40e0253a4a3ed47e0560dd
Desperationis <smarttdiego@gmail.com>
2020-06-13 22:52:08 -0700

    Added test p5.js file

---
+++ TOCYEN/sketch.js
@@ -0,0 +1,22 @@
+let inconsolata;
+function preload() {
+  inconsolata = loadFont('assets/8bitoperator_jve.ttf');
+}
+function setup() {
+  createCanvas(800, 800, WEBGL);
+  textFont(inconsolata);
+  textSize(width / 3);
+  textAlign(CENTER, CENTER);
+}
+function draw() {
+  background(0);
+  let time = millis();
+  text('this ain\'t it chief', (time % 6000) - 3000, 0);
+  
+  rotateX(time / 1000);
+  rotateZ(time / 1234);
+  text('p5.js', 0, 0);
+  rotateX(-time / 1000);
+  rotateZ(-time / 1234);
+
+}
